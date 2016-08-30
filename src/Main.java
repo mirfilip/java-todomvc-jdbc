513b6c34a075995d5818cf7a3d6ecd1409b0feba
Mirosław Filip <miroslaw.filip@schibsted.pl>
2016-08-30 09:54:13 +0200

    Assembly object tree in the Main file

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,26 @@
-import java.io.IOException;
+import org.apache.commons.dbcp2.BasicDataSource;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Server server = new JdkHttpServer(new Application());
+    public static void main(String[] args) throws Exception {
+        String dbUrl = "jdbc:mysql://localhost:3306/todomvc" +
+                "?verifyServerCertificate=false" +
+                "&useSSL=false" +
+                "&useUnicode=true" +
+                "&serverTimezone=UTC";
+
+        BasicDataSource dataSource = new BasicDataSource();
+
+        String dbUsername = "techjump";
+        String dbPassword = "techjump";
+
+        dataSource.setUsername(dbUsername);
+        dataSource.setPassword(dbPassword);
+
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl(dbUrl);
+        dataSource.setInitialSize(5);
+
+        Server server = new JdkHttpServer(new Application(new RdbmsRepository(dataSource)));
 
         server.run();
     }
